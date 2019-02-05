beda91e7bdd53912b9da271384f1a479cf277799
weird-Distortion <ilyalilison@gmail.com>
2019-02-05 12:00:08 +0100

    Main character enum added.

--- src/main/java/printers/EngPrinter.java
+++ src/main/java/printers/EngPrinter.java
@@ -4,7 +4,7 @@ import logic.Picker;
 import scratches.english.EngTime;
 import scratches.english.EngLocations;
 import scratches.english.EngScratch;
-import scratches.english.EngWeapon;
+import scratches.english.EngMainCharacter;
 
 import java.util.List;
 
@@ -17,7 +17,7 @@ public class EngPrinter extends Printer {
 
     public EngPrinter() {
         this.scratches =
-                asList(EngTime.values(), EngLocations.values(), EngWeapon.values());
+                asList(EngTime.values(), EngLocations.values(), EngMainCharacter.values());
     }
 
     @Override
---
+++ src/main/java/scratches/english/EngMainCharacter.java
@@ -0,0 +1,41 @@
+package scratches.english;
+
+public enum EngMainCharacter implements EngScratch {
+    HUMAN("Human"),
+    CYBORG("Cyborg"),
+    DWARF("Dwarf"),
+    AI("Artificial intelligence"),
+    CHILD("Child"),
+    ELF("Elf"),
+    ALIEN("Alien"),
+    NERD("Nerd"),
+    KILLER("Killer"),
+    THIEF("Thief"),
+    GOBLIN("Goblin"),
+    GOD("God"),
+    WRITER("Writer"),
+    PLUMBER("Plumber"),
+    ATHLETE("Athlete"),
+    DEVELOPER("Developer"),
+    GAMER("Gamer"),
+    TEACHER("Teacher"),
+    PROFESSOR("Professor"),
+    KNIGHT("Knight"),
+    VAMPIRE("Vampire"),
+    ZOMBIE("Zombie"),
+    WIZARD("Wizard"),
+    LAWYER("Lawyer"),
+    KING("King"),
+    GOVERNMENT("Government");
+
+    private String mainCharacter;
+
+    EngMainCharacter(String mainCharacter) {
+        this.mainCharacter = mainCharacter;
+    }
+
+    @Override
+    public String toString() {
+        return "the antagonist/protagonist is " + this.mainCharacter;
+    }
+}
